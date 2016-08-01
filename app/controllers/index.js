758d3b56d85910b790b901a0985d35cf379bd888
Mike North <michael.l.north@gmail.com>
2016-08-02 01:46:06 +0800

    ember anchor example

---
+++ app/controllers/index.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import ControllerSupport from 'ember-anchor/mixins/controller-support';
+
+export default Ember.Controller.extend(ControllerSupport, {
+  queryParams: ['anc'],
+  anc: 'first'
+});
