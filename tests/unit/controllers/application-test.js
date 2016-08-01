c223d8ec63902a739796a07d7a28cf44b5aaca4e
Mike North <michael.l.north@gmail.com>
2016-08-02 01:52:14 +0800

    Move ember-anchor component to application.hbs

--- tests/unit/controllers/application-test.js
+++ tests/unit/controllers/application-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('controller:index', 'Unit | Controller | index', {
+moduleFor('controller:application', 'Unit | Controller | application', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
